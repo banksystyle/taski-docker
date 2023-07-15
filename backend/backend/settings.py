108dc3399dfae40766c3b2d3be7a74665c0ac46e
Петр Глебов <banksystyle@yandex.com>
2023-07-15 20:51:47 +0300

    With kittygram

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.30.82', '127.0.0.1', 'localhost', 'gtaski.sytes.net']
 
